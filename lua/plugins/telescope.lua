7cd4e77742b0ac0c86d819c0443606d447b6b825
kunxl-gg <tiwari.25@iitj.ac.in>
2024-02-19 00:35:56 +0530

    Added keymaps to telescope
    Signed-off-by: kunxl-gg <tiwari.25@iitj.ac.in>

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,7 +12,12 @@ return {
 		},
 		config = function()
 			local actions = require("telescope.actions")
-
+			local builtin = require('telescope.builtin')
+			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+			vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+			vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+			
 			require("telescope").setup({
 				defaults = {
 					mappings = {
